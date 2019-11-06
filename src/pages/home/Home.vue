d2ef1b4dff204ea57ddb6f94bc83c78dbdb4b102
ywj-123456 <2683456169@qq.com>
2019-11-06 15:47:27 +0800

    init project

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>hello World</h2>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default{
+    name:'Home'
+}
+</script>
+sss
+<style scoped>
+
+</style>
